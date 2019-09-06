6e4ba69e158d9c90fe02bfa2ac0e76e602f542eb
RAQUEL DA SILVA GONCALVES <398159087@alunos.etec.itaqua>
2019-09-06 15:23:23 -0300

    juntei o site com um servidor

--- Aluno.js
+++ Aluno.js
@@ -1,16 +1,24 @@
-window.onload = function(){
- console.warn("Ola mundo")   
+window.onload = function () {
+    console.warn("Ola mundo")
 
-let meuForm = $("form#formAluno")
-console.log(meuForm)
-meuForm.on("submit",function(event){
-event.preventDefault()
+    let meuForm = $("form#formAluno")
 
-    console.log("Cadastrado")
-    let botao = $("form button")
-    console.log(botao)
+    console.log(meuForm)
+    meuForm.on("submit", function (event) {
+        event.preventDefault()
 
-    let botaoQGira = Ladda.create(botao[0])
-    botaoQGira.start()
-}) 
+        console.log("Cadastrado")
+        let botao = $("form button")
+        console.log(botao)
+
+        let botaoQGira = Ladda.create(botao[0])
+        botaoQGira.start()
+        //fecth coloca o site num servidor
+        //parametro do fetch
+        fetch("http://localhost:8080/sen-api/test.php").then(function (resposta) {
+            return resposta.json()
+        }).then(function (dados) {
+            alert(dados.raquel)
+        })
+    })
 }
